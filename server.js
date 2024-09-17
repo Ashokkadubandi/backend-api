41dc21abeeca51dca58ad68a636f3f7fac488c0e
Ashok <velmaashok765@gmail.com>
2024-09-17 12:23:40 +0000

    authentication

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const path = require("path");
 const dbPath = path.join(__dirname, "userData.db");
+const bcrypt = require("bcrypt");
+const { v4: uuidv4 } = require("uuid");
 let db = null;
 
 const initializeDbAndServer = async () => {
@@ -27,21 +29,48 @@ initializeDbAndServer();
 app.use(express.json());
 
 //GET API method
-app.get("/home", async (request, response) => {
-  const query = `select * from user`;
-  let data = await db.all(query);
+app.get("/userDetails", async (request, response) => {
+  const query = `select * from userInfo`;
+  const data = await db.all(query);
   response.send(data);
 });
 
-//POST API method
-app.post("/create/", async (request, response) => {
-  const { name, Id, role } = request.body;
-  const query = `
-    INSERT INTO USER(id,name,role)
-    VALUES(${Id},"${name}","${role}")`;
+//POST Register API method
 
-  let data = await db.run(query);
-  let msg = `Successfully added the name ${name}`;
-  let arrMsg = [msg];
-  response.send(arrMsg);
+app.post("/register", async (request, response) => {
+  const { username, gender, password } = request.body;
+  const encrypted = await bcrypt.hash(password, 10);
+  const getQuery = `select * from userInfo where username='${username}'`;
+  const data = await db.get(getQuery);
+  if (data === undefined) {
+    const id = uuidv4();
+    const query = `
+          INSERT INTO userInfo(id,username,gender,password)
+          values('${id}','${username}','${gender}','${encrypted}')
+      `;
+    await db.run(query);
+    response.send("Successfully Registered");
+  } else {
+    response.status(400);
+    response.send("User already exists");
+  }
+});
+
+//POST Login API
+app.post("/login", async (request, response) => {
+  const { username, password } = request.body;
+  const getQuery = `select * from userInfo where username = '${username}'`;
+  const getData = await db.get(getQuery);
+  if (getData === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isMatch = await bcrypt.compare(password, getData.password);
+    if (isMatch === true) {
+      response.send("Login Success");
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
+  }
 });
